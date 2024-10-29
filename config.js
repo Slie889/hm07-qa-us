4b438e47420a3631c70a2c6db98ccc6e6941cf81
slie889 <suv99@live.com>
2024-10-29 00:17:47 -0700

    getHandlers.test.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "paste_your_api_url_here"
+    API_URL: "https://cnt-40141f06-1e76-446b-a3f3-7808d6266440.containerhub.tripleten-services.com"
 }
--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -1,10 +1,13 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test('Should return 200 status code....', async () => {
+	let actualStatusCode;
 	try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`);
+		const response = await fetch(`${config.API_URL}/api/v1/kits/1`);
+		actualStatusCode = response.status;
 	} catch (error) {
 		console.error(error);
 	}
+	expect(actualStatusCode).toBe(200);
 });
