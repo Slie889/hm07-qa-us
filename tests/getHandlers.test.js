f5340bfad55ea57617bc8df789bc0e81d5764860
slie889 <slie889@users.noreply.github.com>
2024-10-30 00:04:40 -0700

    Update getHandlers.test.js

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -4,7 +4,7 @@ const config = require('../config');
 test('Should return 200 status code....', async () => {
 	let actualStatusCode;
 	try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/`);
+		const response = await fetch(`${config.API_URL}/api/v1/kits/1`);
 		actualStatusCode = response.status;
 	} catch (error) {
 		console.error(error);
