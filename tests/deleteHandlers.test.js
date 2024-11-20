9b2e50c8e8a2d857e80e419d6773eaef6391f3de
slie889 <slie889@users.noreply.github.com>
2024-11-19 21:15:16 -0800

    Update deleteHandlers.test.js

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,10 +1,19 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
+const requestBody = {
+	"productsList": [
+		{
+		  "id": 4,
+		  "quantity": 1
+		}
+	  ]
+}
+
 test('Status code should be 200', async () => {
 	let actualStatusCode;
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/2`, {
+		const response = await fetch(`${config.API_URL}/api/v1/orders/1`, {
 			method: 'DELETE',
 		});
 		actualStatusCode = response.status;
@@ -13,4 +22,22 @@ test('Status code should be 200', async () => {
 	}
 
 	expect(actualStatusCode).toBe(200);
+});
+
+test('Response body should contain ......', async () => {
+    let actualResponseBody;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/orders/1`, {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+		actualResponseBody = await response.json();
+	} catch (error) {
+		console.error(error);
+	}
+
+	expect(actualResponseBody["courierService"]).toBe("Order and Go");
 });
