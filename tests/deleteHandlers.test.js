9f97e78fe61279d4c59bf7fc2e86f731e389ead3
slie889 <slie889@users.noreply.github.com>
2024-11-24 14:31:38 -0800

    Update deleteHandlers.test.js

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -2,19 +2,14 @@
 const config = require('../config');
 
 const requestBody = {
-	"id": 6,
-	"productsList": [
-		{
-		  "id": 6,
-		  "quantity": 1
-		}
-	  ]
+	"name": "Testing",
+	"cardId": 1 
 }
 	
 test('Status code should be 200', async () => {
 	let actualStatusCode;
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/orders/${6}`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
 			method: 'DELETE',
 		});
 		actualStatusCode = response.status;
@@ -28,7 +23,7 @@ test('Status code should be 200', async () => {
 test('Response body should contain ......', async () => {
     let actualResponseBody;
 	try {
-		const response = await fetch(`${config.API_URL}/api/v1/orders/${6}`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
 			method: 'DELETE',
 			headers: {
 			'Content-Type': 'application/json'
